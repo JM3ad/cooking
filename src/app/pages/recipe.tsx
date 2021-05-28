767db5dc5b6ecd1cac6de78b31a969b524b83d11
Jack Mead <jack-mead@hotmail.co.uk>
2021-05-28 13:16:10 +0100

    Small improvement to the recipe layout page

--- src/app/pages/recipe.tsx
+++ src/app/pages/recipe.tsx
@@ -1,6 +1,7 @@
 import { Recipe } from 'app/models/recipe';
 import React from 'react';
 import 'app/pages/Pages.scss';
+import 'app/pages/Recipes.scss';
 
 type RecipeProps = {
     recipe: Recipe;
@@ -52,7 +53,8 @@ const RecipeDisplay: React.FC<RecipeProps> = (props: RecipeProps) => {
     return (
         <div className="app-page">
             <div className="recipe">
-                <a href={recipe.url} className="recipe-link">Based on {recipe.name}</a>
+                <h1 className="recipe-title">{recipe.name}</h1>
+                <a href={recipe.url} className="recessive-link">Click for original recipe</a>
                 <IngredientsDisplay ingredients={recipe.ingredients} />
                 <MethodDisplay method={recipe.method} />
             </div>
